28c3ab764a334fe5dfc135b08bf846894764a569
MattSouuza <matheus_ss02@hotmail.com>
2023-06-28 20:54:38 -0300

    new: home rolezeiro page almost done

--- src/pages/home-rolezeiro/index.tsx
+++ src/pages/home-rolezeiro/index.tsx
@@ -6,13 +6,21 @@ const HomeRolezeiro = () => {
         <main className="home-rolezeiro">
             <header className="home-rolezeiro__header">
                 <span className="header__go-back">Voltar</span>
-                <h3 className="header__go-anounce">Quero anunciar!</h3>
+                <p className="header__go-anounce">Quero anunciar!</p>
             </header>
 
-            <section className="home-rolezeiro__events">
-                <h1>Eventos do Momento!</h1>
+            <section className="home-rolezeiro__content">
 
-                <EventCard name="Anime Friends" date={new Date()} confirmedUsers={["id"]} currentHomePageType="rolezeiro"/>
+                <section className="content__title">
+                    <h1>Eventos do Momento!</h1>
+                    <p>saia do tédio agora ou ajude outros a sairem! Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </section>
+
+                <section className="content__events">
+                    <EventCard name="Anime Friends" date={new Date()} confirmedUsers={["id"]} currentHomePageType="rolezeiro" />
+                    <EventCard name="Bailão" date={new Date()} confirmedUsers={["id"]} currentHomePageType="rolezeiro" />
+                    <EventCard name="Show da Gloria" date={new Date()} confirmedUsers={["id"]} currentHomePageType="rolezeiro" />
+                </section>
             </section>
         </main>
     );
